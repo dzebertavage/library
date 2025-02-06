ee36aea846cd72b682fdc672b1d769e12408e304
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2025-02-05 19:40:40 -0500

    Add basic grid layout for library app

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [
+    {"title": "First Book",
+        "author": "First Author"
+    },
+    {
+        "title": "Second Book",
+        "author": "Second Author"
+    }
+];
+
+// function Book(title, author) {
+//     this.title = title;
+//     this.author = author;
+// }
+
+function addBookToLibrary() {
+
+}
